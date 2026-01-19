db87602dda40126a0a83e6481fb4ca1079536dd0
Brandon Ho <bho36@gatech.edu>
2026-01-19 09:25:14 -0500

    Create custom visualization for plotting roadmap & scripts for PRM & graph sampler

--- scripts/run_prm.py
+++ scripts/run_prm.py
@@ -4,7 +4,7 @@ random.seed(0)
 import numpy as np
 np.random.seed(0)
 
-from path_planning.global_planner.sample_search.prm2 import PRM
+from path_planning.global_planner.sample_search.prm import PRM
 from path_planning.utils.util import read_grid_from_yaml
 from python_motion_planning.common import *
 from python_motion_planning.path_planner import *
@@ -13,22 +13,22 @@ from python_motion_planning.controller import *
 if __name__ == "__main__":
 
 
-    # map_ =read_grid_from_yaml('path_planning/maps/2d/2d.yaml')
-    # map_.inflate_obstacles(radius=3)
-    # start = (5, 5)
-    # goal = (45, 25)
-    # map_.type_map[start] = TYPES.START
-    # map_.type_map[goal] = TYPES.GOAL
+    map_ =read_grid_from_yaml('path_planning/maps/2d/2d.yaml')
+    map_.inflate_obstacles(radius=3)
+    start = (5, 5)
+    goal = (45, 25)
+    map_.type_map[start] = TYPES.START
+    map_.type_map[goal] = TYPES.GOAL
 
-    # planner = PRM(map_=map_, start=start, goal=goal,num_sample=1000,min_edge_len=1)
-    # path, path_info = planner.plan()
+    planner = PRM(map_=map_, start=start, goal=goal,num_sample=1000,min_edge_len=1)
+    path, path_info = planner.plan()
 
-    # vis = Visualizer2D()
-    # vis.plot_grid_map(map_)
-    # vis.plot_path(path)       
-    # vis.plot_expand_tree(path_info["expand"])
-    # vis.savefig('prm.png')
-    # vis.close()
+    vis = Visualizer2D()
+    vis.plot_grid_map(map_)
+    vis.plot_path(path)       
+    vis.plot_expand_tree(path_info["expand"])
+    vis.savefig('figs/prm/prm_2d.png')
+    vis.close()
 
 
     map_ =read_grid_from_yaml('path_planning/maps/3d/3d.yaml')
@@ -45,5 +45,8 @@ if __name__ == "__main__":
     vis.plot_grid_map(map_)
     vis.plot_path(path)       
     vis.plot_expand_tree(path_info["expand"])
-    vis.savefig('prm.png')
+    
+    # For 3D visualization, PyVista requires show() before savefig()
+    # Call show() first to initialize the renderer, then save
     vis.show()
+    vis.savefig('figs/prm/prm_3d.png')
