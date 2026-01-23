364e5e078aa0cb523d9f74bab69b2cf2926d49d3
Brandon Ho <bho36@gatech.edu>
2026-01-23 12:04:28 -0500

    Modified a_star and cbs to be more efficient and deterministic

--- path_planning/multi_agent_planner/centralized/cbs/a_star.py
+++ path_planning/multi_agent_planner/centralized/cbs/a_star.py
@@ -5,13 +5,16 @@ AStar search
 author: Ashwin Bose (@atb033)
 
 """
-
+import heapq
 class AStar():
     def __init__(self, env):
         self.agent_dict = env.agent_dict
         self.admissible_heuristic = env.admissible_heuristic
         self.is_at_goal = env.is_at_goal
         self.get_neighbors = env.get_neighbors
+        # Internal counter used as a deterministic tie-breaker in the
+        # priority queue when f-scores are equal.
+        self._counter = 0
 
     def reconstruct_path(self, came_from, current):
         total_path = [current]
@@ -27,8 +30,13 @@ class AStar():
         initial_state = self.agent_dict[agent_name]["start"]
         step_cost = 1
         
+
         closed_set = set()
-        open_set = {initial_state}
+        # Use a heap-based priority queue for deterministic selection of the
+        # next node to expand instead of a raw set whose iteration order can
+        # vary. Each heap entry is (f_score, counter, state).
+        open_heap = []
+        open_set = set()
 
         came_from = {}
 
@@ -39,9 +47,16 @@ class AStar():
 
         f_score[initial_state] = self.admissible_heuristic(initial_state, agent_name)
 
-        while open_set:
-            temp_dict = {open_item:f_score.setdefault(open_item, float("inf")) for open_item in open_set}
-            current = min(temp_dict, key=temp_dict.get)
+        # Initialize open list
+        heapq.heappush(open_heap, (f_score[initial_state], self._counter, initial_state))
+        self._counter += 1
+        open_set.add(initial_state)
+
+        while open_heap:
+            # Pop the best node; skip any stale entries no longer in open_set.
+            _, _, current = heapq.heappop(open_heap)
+            if current not in open_set:
+                continue
 
             if self.is_at_goal(current, agent_name):
                 return self.reconstruct_path(came_from, current)
@@ -57,14 +72,17 @@ class AStar():
                 
                 tentative_g_score = g_score.setdefault(current, float("inf")) + step_cost
 
-                if neighbor not in open_set:
-                    open_set |= {neighbor}
-                elif tentative_g_score >= g_score.setdefault(neighbor, float("inf")):
-                    continue
+                if neighbor not in open_set or tentative_g_score < g_score.setdefault(neighbor, float("inf")):
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = g_score[neighbor] + self.admissible_heuristic(neighbor, agent_name)
 
-                came_from[neighbor] = current
+                    if neighbor not in open_set:
+                        open_set.add(neighbor)
 
-                g_score[neighbor] = tentative_g_score
-                f_score[neighbor] = g_score[neighbor] + self.admissible_heuristic(neighbor, agent_name)
+                    # Push (or push updated) entry into the heap. Stale entries
+                    # for this neighbor will simply be ignored when popped.
+                    heapq.heappush(open_heap, (f_score[neighbor], self._counter, neighbor))
+                    self._counter += 1
         return False
 
--- path_planning/multi_agent_planner/centralized/cbs/cbs.py
+++ path_planning/multi_agent_planner/centralized/cbs/cbs.py
@@ -7,24 +7,37 @@ description: This file implements the Conflict-based search algorithm for multi-
 
 from path_planning.common.environment.node import Node
 from path_planning.multi_agent_planner.centralized.cbs.a_star import AStar
-from python_motion_planning.path_planner.graph_search.a_star import AStar as AStar2
+import time
 from math import fabs
 from itertools import combinations
 from copy import deepcopy
 
+class Location(object):
+    def __init__(self, point:tuple = None):
+        self.point = tuple(point) if point is not None else None
+    def __eq__(self, other):
+        return self.point == other.point
+    def __str__(self):
+        return str(self.point)
+    def __len__(self):
+        return len(self.point)
+    def __getitem__(self, index):
+        return self.point[index]
+    def __add__(self, other):
+        return Location(tuple(self.point + other.point))
 
 class State(object):
-    def __init__(self, time, node: Node):
+    def __init__(self, time, location):
         self.time = time
-        self.node = node
+        self.location = location
     def __eq__(self, other):
-        return self.time == other.time and self.node == other.node
+        return self.time == other.time and self.location == other.location
     def __hash__(self):
-        return hash(str(self.time)+str(self.node))
+        return hash(str(self.time)+str(self.location))
     def is_equal_except_time(self, state):
-        return self.node == state.node
+        return self.location == state.location
     def __str__(self):
-        return str((self.time, self.node))
+        return str((self.time, self.location))
 
 class Conflict(object):
     VERTEX = 1
@@ -36,37 +49,37 @@ class Conflict(object):
         self.agent_1 = ''
         self.agent_2 = ''
 
-        self.node_1 = Node(None,None,0,0)
-        self.node_2 = Node(None,None,0,0)
+        self.location_1 = Location()
+        self.location_2 = Location()
 
     def __str__(self):
         return '(' + str(self.time) + ', ' + self.agent_1 + ', ' + self.agent_2 + \
-             ', '+ str(self.node_1) + ', ' + str(self.node_2) + ')'
+             ', '+ str(self.location_1) + ', ' + str(self.location_2) + ')'
 
 class VertexConstraint(object):
-    def __init__(self, time, node):
+    def __init__(self, time, location):
         self.time = time
-        self.node = node
+        self.location = location
 
     def __eq__(self, other):
-        return self.time == other.time and self.node == other.node
+        return self.time == other.time and self.location == other.location
     def __hash__(self):
-        return hash(str(self.time)+str(self.node))
+        return hash(str(self.time)+str(self.location))
     def __str__(self):
-        return '(' + str(self.time) + ', '+ str(self.node) + ')'
+        return '(' + str(self.time) + ', '+ str(self.location) + ')'
 
 class EdgeConstraint(object):
-    def __init__(self, time, node_1, node_2):
+    def __init__(self, time, location_1, location_2):
         self.time = time
-        self.node_1 = node_1
-        self.node_2 = node_2
+        self.location_1 = location_1
+        self.location_2 = location_2
     def __eq__(self, other):
-        return self.time == other.time and self.node_1 == other.node_1 \
-            and self.node_2 == other.node_2
+        return self.time == other.time and self.location_1 == other.location_1 \
+            and self.location_2 == other.location_2
     def __hash__(self):
-        return hash(str(self.time) + str(self.node_1) + str(self.node_2))
+        return hash(str(self.time) + str(self.location_1) + str(self.location_2))
     def __str__(self):
-        return '(' + str(self.time) + ', '+ str(self.node_1) +', '+ str(self.node_2) + ')'
+        return '(' + str(self.time) + ', '+ str(self.location_1) +', '+ str(self.location_2) + ')'
 
 class Constraints(object):
     def __init__(self):
@@ -93,24 +106,23 @@ class Environment(object):
         self.constraint_dict = {}
 
         self.a_star = AStar(self)
-        self.a_star2 = AStar2(graph_map,None,None)
 
     def get_neighbors(self, state):
         neighbors = []
-        nodes = self.graph_map.get_neighbors(state.node)
-        
+        node = Node(tuple(state.location.point))
+        nodes = self.graph_map.get_neighbors(node)
         # Wait action
-        n = State(state.time + 1, state.node)
-        if self.state_valid(n):
+        n = State(state.time + 1,  state.location)
+        if self.state_valid(n) and self.transition_valid(state, n):
             neighbors.append(n)
 
         # Move action
         for node in nodes:
-            n = State(state.time + 1, node)
-            if self.state_valid(n):
+            n = State(state.time + 1, Location(tuple(node.current)))
+            if self.state_valid(n) and self.transition_valid(state, n):
                 neighbors.append(n)
-
         return neighbors
+
 
     def get_first_conflict(self, solution):
         max_t = max([len(plan) for plan in solution.values()])
@@ -122,7 +134,7 @@ class Environment(object):
                 if state_1.is_equal_except_time(state_2):
                     result.time = t
                     result.type = Conflict.VERTEX
-                    result.node_1 = state_1.node
+                    result.location_1 = state_1.location
                     result.agent_1 = agent_1
                     result.agent_2 = agent_2
                     return result
@@ -139,15 +151,15 @@ class Environment(object):
                     result.type = Conflict.EDGE
                     result.agent_1 = agent_1
                     result.agent_2 = agent_2
-                    result.node_1 = state_1a.node
-                    result.node_2 = state_1b.node
+                    result.location_1 = state_1a.location
+                    result.location_2 = state_1b.location
                     return result
         return False
 
     def create_constraints_from_conflict(self, conflict):
         constraint_dict = {}
         if conflict.type == Conflict.VERTEX:
-            v_constraint = VertexConstraint(conflict.time, conflict.node_1)
+            v_constraint = VertexConstraint(conflict.time, conflict.location_1)
             constraint = Constraints()
             constraint.vertex_constraints |= {v_constraint}
             constraint_dict[conflict.agent_1] = constraint
@@ -157,8 +169,8 @@ class Environment(object):
             constraint1 = Constraints()
             constraint2 = Constraints()
 
-            e_constraint1 = EdgeConstraint(conflict.time, conflict.node_1, conflict.node_2)
-            e_constraint2 = EdgeConstraint(conflict.time, conflict.node_2, conflict.node_1)
+            e_constraint1 = EdgeConstraint(conflict.time, conflict.location_1, conflict.location_2)
+            e_constraint2 = EdgeConstraint(conflict.time, conflict.location_2, conflict.location_1)
 
             constraint1.edge_constraints |= {e_constraint1}
             constraint2.edge_constraints |= {e_constraint2}
@@ -175,20 +187,19 @@ class Environment(object):
             return solution[agent_name][-1]
 
     def state_valid(self, state):
-
-        if self.graph_map.in_collision(state.node.current):
+        if self.graph_map.in_collision_point(state.location.point):
             return False
-        return  VertexConstraint(state.time, state.node) not in self.constraints.vertex_constraints 
+        return VertexConstraint(state.time, state.location) not in self.constraints.vertex_constraints
 
     def transition_valid(self, state_1, state_2):
-        return EdgeConstraint(state_1.time, state_1.node, state_2.node) not in self.constraints.edge_constraints
+        return EdgeConstraint(state_1.time, state_1.location, state_2.location) not in self.constraints.edge_constraints
 
     def is_solution(self, agent_name):
         pass
 
     def admissible_heuristic(self, state, agent_name):
         goal = self.agent_dict[agent_name]["goal"]
-        return sum([fabs(state.node.current[i] - goal.node.current[i]) for i in range(len(state.node.current))])
+        return sum([fabs(state.location[i] - goal.location[i]) for i in range(len(state.location))])
 
 
     def is_at_goal(self, state, agent_name):
@@ -198,18 +209,8 @@ class Environment(object):
     def make_agent_dict(self):
         for name in self.agents:
             agent = self.agents[name]
-            start_node = Node(agent['start'],None,0,0)
-            goal_node = Node(agent['goal'],None,0,0)
-            
-            if start_node in self.graph_map.node_index_list:
-                start_index= self.graph_map.node_index_list[start_node]
-                start_node = self.graph_map.nodes[start_index]
-            if goal_node in self.graph_map.node_index_list:
-                goal_index = self.graph_map.node_index_list[goal_node]
-                goal_node = self.graph_map.nodes[goal_index]
-
-            start_state = State(0, start_node)
-            goal_state = State(0, goal_node)
+            start_state = State(0, Location(agent['start']))
+            goal_state = State(0, Location(agent['goal']))
             
             self.agent_dict.update({name:{'start':start_state, 'goal':goal_state}})
 
@@ -237,16 +238,48 @@ class HighLevelNode(object):
         return self.solution == other.solution and self.cost == other.cost
 
     def __hash__(self):
-        return hash((self.cost))
+        return hash(self.cost)
 
     def __lt__(self, other):
         return self.cost < other.cost
 
 class CBS(object):
-    def __init__(self, environment):
+    def __init__(self, environment, time_limit: float | None = None, max_iterations: int | None = None):
+        """
+        :param environment: Environment instance
+        :param time_limit: Optional wall-clock time limit (in seconds) for the
+                           high-level CBS search. If None, no time limit is
+                           enforced. If exceeded, the search terminates early
+                           and returns an empty solution.
+        """
         self.env = environment
-        self.open_set = set()
+        # Use a priority queue (heap) for deterministic expansion order
+        # instead of an unordered set whose iteration order can vary
+        # between runs and Python versions.
+        #
+        # Each heap entry is a tuple: (cost, HighLevelNode)
+        self.open_heap = []
+        self.open_set = set()   # for fast membership checks
         self.closed_set = set()
+        self.time_limit = time_limit
+        self.max_iterations = max_iterations
+        self.iterations = 0
+
+    def _push_open(self, node):
+        """Push a node into the open list (heap) in a deterministic way."""
+        import heapq
+        heapq.heappush(self.open_heap, (node.cost, node))
+        self.open_set.add(node)
+
+    def _pop_open(self):
+        """Pop the next node from the open list, skipping any stale entries."""
+        import heapq
+        while self.open_heap:
+            _, node = heapq.heappop(self.open_heap)
+            if node in self.open_set:
+                self.open_set.remove(node)
+                return node
+        return None
     def search(self):
         start = HighLevelNode()
         # TODO: Initialize it in a better way
@@ -258,22 +291,32 @@ class CBS(object):
             return {}
         start.cost = self.env.compute_solution_cost(start.solution)
 
-        self.open_set |= {start}
+        # Initialize open list with the start node
+        self._push_open(start)
 
-        while self.open_set:
-            P = min(self.open_set)
-            self.open_set -= {P}
+         
+        st = time.time()
+        while self.open_heap:
+            P = self._pop_open()
+            if P is None:
+                break
             self.closed_set |= {P}
+
+            # Enforce optional wall-clock time limit on the high-level search
+            if self.time_limit is not None and (time.time() - st) > self.time_limit:
+                print(f"Search terminated: time limit of {self.time_limit} seconds exceeded.")
+                return {}
+
+            if self.max_iterations is not None and self.iterations >= self.max_iterations:
+                print(f"Search terminated: max iterations of {self.max_iterations} reached.")
+                return {}
 
             self.env.constraint_dict = P.constraint_dict
             conflict_dict = self.env.get_first_conflict(P.solution)
             if not conflict_dict:
                 print("solution found")
-
                 return self.generate_plan(P.solution)
-
             constraint_dict = self.env.create_constraints_from_conflict(conflict_dict)
-
             for agent in constraint_dict.keys():
                 new_node = deepcopy(P)
                 new_node.constraint_dict[agent].add_constraint(constraint_dict[agent])
@@ -283,11 +326,10 @@ class CBS(object):
                 if not new_node.solution:
                     continue
                 new_node.cost = self.env.compute_solution_cost(new_node.solution)
-
                 # TODO: ending condition
-                if new_node not in self.closed_set:
-                    self.open_set |= {new_node}
-
+                if new_node not in self.closed_set and new_node not in self.open_set:
+                    self._push_open(new_node)
+            self.iterations += 1
         return {}
 
     def generate_plan(self, solution):
@@ -295,9 +337,11 @@ class CBS(object):
         for agent, path in solution.items():
             path_dict_list = []
             for state in path:
-                if len(state.node.current) == 2:
-                    path_dict_list.append({'t':state.time, 'x':state.node.current[0], 'y':state.node.current[1]})
+                if len(state.location) == 2:
+                    path_dict_list.append({'t':state.time, 'x':state.location[0], 'y':state.location[1]})
+                elif len(state.location) == 3:
+                    path_dict_list.append({'t':state.time, 'x':state.location[0], 'y':state.location[1], 'z':state.location[2]})
                 else:
-                    path_dict_list.append({'t':state.time, 'x':state.node.current[0], 'y':state.node.current[1], 'z':state.node.current[2]})
+                    raise ValueError(f"Invalid location dimension: {len(state.location)}")
             plan[agent] = path_dict_list
         return plan
