5c11b140a4902b972b5064d59cfd3f7d84d3ac05
Brandon Ho <bho36@gatech.edu>
2026-02-04 19:28:34 -0500

    Add a load and save option for the dataloader & create a dataset getter function

--- path_planning/gnn/dataloader.py
+++ path_planning/gnn/dataloader.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from typing import List,Tuple
 from multiprocessing import Pool, cpu_count
 from path_planning.data_generation.target_gen import generate_roadmap,generate_target_space
-
+import os
 def _load_single_graph(files: Tuple[Path, Path]) -> HeteroData:
     """
     Load and process a single graph file (worker function for multiprocessing).
@@ -47,15 +47,26 @@ def _load_single_graph(files: Tuple[Path, Path]) -> HeteroData:
 
 class GraphDataset(InMemoryDataset):  
     def __init__(self,
-                data_files:List[Tuple[Path,Path]],
+                data_files:List[Path],
+                load_file:Path = None,
+                save_file:Path = '',
                 root:Optional[str] = None,
                 transform: Optional[Callable] = None,
                 pre_transform: Optional[Callable] = None,
                 pre_filter: Optional[Callable] = None,
                 num_workers: Optional[int] = 1):
-        super().__init__(root, transform, pre_transform, pre_filter)
-
         self.data_files = data_files
+        self.load_file = load_file
+        self.save_file = save_file if len(save_file) > 0 else os.path.join(root, 'data.pt') if root is not None else 'data.pt'
+        super().__init__(root, transform, pre_transform, pre_filter)
+        
+        
+
+        if load_file is not None:
+            if os.path.exists(load_file):
+                self.load(load_file)
+                return
+            print(f"Loading {load_file} does not exist, processing data")
 
         if len(data_files) == 0:
             raise ValueError("No data files provided")
@@ -87,8 +98,49 @@ class GraphDataset(InMemoryDataset):
     def raw_file_names(self):
         return self.data_files 
 
+    @property
+    def processed_file_names(self):
+        return [self.save_paths]
+
+    def save(self):
+        torch.save((self.data, self.slices), self.save_file)
 
 def create_graph_dataset_loader(paths:List[Path],config:dict):
+    data_files = []
+
+    #Iterate over all paths
+    for path in paths:
+        path = Path(path)
+        cases = sorted([d for d in path.iterdir() if d.is_dir() and d.name.startswith("case_")])
+        if not cases:
+            print("No cases found to process")
+            return
+
+        #Iterate over all cases
+        for case_dir in cases:
+            road_map_type = config["road_map_type"] if "road_map_type" in config else "planar"
+            target_space = config["target_space"] if "target_space" in config else "binary"
+            road_map_type_name = generate_roadmap(road_map_type, None, [], True)
+            roadmap_dir = case_dir / "samples"  / f"{road_map_type_name}" 
+            _,y_type_name = generate_target_space(target_space,None,None,True)
+            if not roadmap_dir.exists():
+                continue
+            graphs_dirs = sorted([d for d in roadmap_dir.iterdir()], key = lambda x: int(x.name.split('_')[-1]))
+
+            #Iterate over all graphs
+            for ii in range(len(graphs_dirs)):
+                # Check if graph file exists
+                graph_dir = graphs_dirs[ii]
+                graph_file = graph_dir / f'graph.npz'
+                target_file = graph_dir / f'target_{y_type_name}.npy'
+                assert graph_file.exists(), f"{graph_file} does not exist"
+                assert target_file.exists(), f"{target_file} does not exist"
+                data_files.append((graph_file,target_file))
+    return data_files
+
+
+
+def get_graph_dataset_file_paths(paths:List[Path],config:dict):
     data_files = []
 
     #Iterate over all paths
